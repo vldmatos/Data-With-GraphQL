a6e19f67e8325c17535ae408149842e9cabe6685
vldmatos <vladimir.matos@outlook.com>
2021-03-05 20:26:13 -0300

    refactor: add schemas

---
+++ Server/Schemas/CarSchema.cs
@@ -0,0 +1,19 @@
+﻿using GraphQL.Types;
+using Server.Domain.Entities;
+
+namespace Server.Schemas
+{
+	public class CarSchema : ObjectGraphType<Car>
+	{
+		public CarSchema()
+		{
+			Name = nameof(Car);
+
+			Field(car => car.Model).Description("Model of Car");
+			Field(car => car.Color).Description("Car's color");
+			Field(car => car.Manufacturer).Description("Manufacturer of car");
+			Field(car => car.Year).Description("Year of car");
+			Field(car => car.Price).Description("Price of car");
+		}
+	}
+}
---
+++ Server/Schemas/CustomerSchema.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using Server.Domain.Entities;
+
+namespace Server.Schemas
+{
+	public class CustomerSchema : ObjectGraphType<Customer>
+	{
+		public CustomerSchema()
+		{
+			Name = nameof(Customer);
+
+			Field(customer => customer.Register).Description("Customer's register");
+			Field(customer => customer.FirstName).Description("Customer's First Name");
+			Field(customer => customer.LastName).Description("Customer's Last Name");
+			Field(customer => customer.Age).Description("Customer's Age");
+		}
+	}
+}
